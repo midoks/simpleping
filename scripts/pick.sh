28b6a7ae8c0ecc12adabeb315cbc07c206ea7503
Mr Chen <midoks@163.com>
2024-07-10 22:48:36 +0800

    Update pick.sh

--- scripts/pick.sh
+++ scripts/pick.sh
@@ -36,28 +36,9 @@ build_app(){
 	export CGO_ENABLED=1 GOOS=$1 GOARCH=$2
 	echo "cd $rootPath && go build main.go"
 
-	export CC=x86_64-linux-musl-gcc
+	
 	echo "arch:$2"
-	if [ $2 == "amd64" ]; then
-		echo "CC=x86_64-linux-musl-gcc"
-		export CC=x86_64-linux-musl-gcc
-	fi
-
-	if [ $2 == "386" ]; then
-		echo "CC=i486-linux-musl-gcc"
-		export CC=i486-linux-musl-gcc
-	fi
-
-	if [ $2 == "arm64" ]; then
-		echo "CC=aarch64-linux-musl-gcc"
-		export CC=aarch64-linux-musl-gcc
-	fi
-
-	if [ $2 == "arm" ]; then
-		echo "CC=arm-linux-musl-gcc"
-		export CC=arm-linux-musleabi-gcc
-	fi
-
+	
 	# export CGO_ENABLED=1 GOOS=linux GOARCH=amd64
 	if [ $1 == "darwin" ]; then
 		echo "go build -o ${PACK_NAME} -v -ldflags '${LDFLAGS}'"
@@ -66,6 +47,27 @@ build_app(){
 	fi
 
 	if [ $1 == "linux" ]; then
+		export CC=x86_64-linux-musl-gcc
+		if [ $2 == "amd64" ]; then
+			echo "CC=x86_64-linux-musl-gcc"
+			export CC=x86_64-linux-musl-gcc
+		fi
+
+		if [ $2 == "386" ]; then
+			echo "CC=i486-linux-musl-gcc"
+			export CC=i486-linux-musl-gcc
+		fi
+
+		if [ $2 == "arm64" ]; then
+			echo "CC=aarch64-linux-musl-gcc"
+			export CC=aarch64-linux-musl-gcc
+		fi
+
+		if [ $2 == "arm" ]; then
+			echo "CC=arm-linux-musl-gcc"
+			export CC=arm-linux-musleabi-gcc
+		fi
+
 		export CGO_LDFLAGS="-static"
 		echo "go build -o ${PACK_NAME} -v -ldflags '${LDFLAGS}' main.go "
 		cd $rootPath && go build -v -ldflags "${LDFLAGS}" -o ${PACK_NAME} main.go
@@ -78,13 +80,13 @@ build_app(){
 	cp -rf $rootPath/README.md $rootPath/tmp/build
 
 	cd $rootPath/tmp/build && xattr -c * && rm -rf ./*/.DS_Store && rm -rf ./*/*/.DS_Store
-	cd $rootPath/tmp/build && tar -zcvf $rootPath/tmp/package/${PACK_NAME}_${VERSION}_$1_$2.tar.gz ./
+	cd $rootPath/tmp/build && tar -zcvf $rootPath/tmp/package/${PACK_NAME}_$1_$2.tar.gz ./
 }
 
 golist=`go tool dist list`
 echo $golist
 
 # build_app linux amd64
-build_app linux arm64
-# build_app darwin amd64
+# build_app linux arm64
+build_app darwin amd64
 # build_app darwin arm64
